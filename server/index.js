c7aa431d19e48eea6c9b554279e8effd0d8a4195
Jopkins <sergebrew@gmail.com>
2022-03-13 19:49:56 +0300

    registration logic

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
-// "express", "mongoose", "config", "nodemon -D" modules
+// "express", "mongoose", "config", "nodemon -D", "bcryptjs", "express-validator" modules
 
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 
+const authRouter = require('./routes/auth.routes');
+
 const app = express();
 const PORT = config.get('serverPort')
+
+app.use(express.json());
+app.use("/api/auth", authRouter);
 
 const start = async () => {
   try {
